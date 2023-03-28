0a8e51486dae641092fdce24953ddf0f3dbcdc3e
Nelexiad <daniele.danna96@gmail.com>
2023-03-28 23:27:03 +0200

    css-adjust|images|navbar|banner

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const listFilm = document.getElementById("film-list");
 const filmDetails = document.getElementById("film-details");
+const navbar = document.getElementById("navbar").classList;
 
 let result = [];
 let filmNames = {};
@@ -20,14 +21,14 @@ const apiMovies = async () => {
     for (let i = 0; i < 10; i++) {
       const current = result.results[i];
 
-      const itemHTML = `<div id="current-film${i}" class="flex pt-24 relative" >
+      const itemHTML = `<div id="current-film${i}" class="flex  w-5/6 relative border-4 border-red-500 rounded-md pt-2 pb-2 text-black" >
         <div class="pl-5">
           <img class="" src="https://image.tmdb.org/t/p/w185/${current.poster_path}"/>
         </div>
-        <div class="flex flex-col justify-center items-start gap-y-14 font-bold pl-5">
-          <div>${current.original_title}</div>
-          <div class="flex pl-5" id="trama:${current.original_title}"       onclick="showDetails('${current.original_title}')">Trama</div>
-          <div class="hidden max-w-md h-20 border-2 border-red-800 overflow-y-scroll absolute bottom-6 right-64" id="details:${current.original_title}">${current.overview}</div>
+        <div class="flex flex-col justify-center  w-full  gap-10 font-bold pl-5">
+          <div class="font-extrabold text-2xl ">${current.original_title}</div>
+          <div class="flex gap-10" id="trama:${current.original_title}"       onclick="showDetails('${current.original_title}')">Trama<div class="hidden  max-w-md  border-2 border-red-800 rounded-2xl bg-custom-yellow shadow-red-500 shadow-2xl   h-max text-black" id="details:${current.original_title}">${current.overview}</div></div>
+          
         </div>
         
       </div>`;
@@ -46,3 +47,14 @@ const apiMovies = async () => {
 };
 
 apiMovies();
+
+window.addEventListener("scroll", function (event) {
+  let top = window.scrollY;
+  if (top > 350) {
+    navbar.add("fixed");
+    navbar.add("top-0");
+  } else if (top < 350) {
+    navbar.remove("fixed");
+    navbar.remove("top-0");
+  }
+});
